5fc514140224f8bb8763749aaa090ea3e4083555
Nate Islip <39868505+nislip@users.noreply.github.com>
2022-01-16 09:00:03 -0800

    Update rmds_application_nislip.py

--- rmds_application_nislip.py
+++ rmds_application_nislip.py
@@ -5,24 +5,38 @@ import streamlit as st
 
 st.title("RMDS Competiion")
 
-tabs = st.sidebar.selectbox("Portfolio Phase: ", ["EDA", "Forecast and Portfolio Performance"])
+tabs = st.sidebar.selectbox("Portfolio Phase: ", ["EDA", "Forecast and Portfolio Performance", "Appendix"])
 
+# Exploratory Data Analysis
 if tabs == "EDA":
     
     st.subheader("Upload your Portfolio data: ")
     investor_data = st.file_uploader('Upload a CSV file')
     
+    # Portfolio Overview
+    '''
+    Metrics: Key Performance Indicators for investor portfolio 
+    Vizualizations:
+            (1) Limit the size of the data to maybe 2000 rows/observations
+    '''
     st.subheader("Portfolio Overview")
     
+    # Key Performance Indicators
     col1, col2, col3, col4 = st.columns(4) 
     col1.metric("Most valued Stock", 70, delta = 1)
     col2.metric("Least valued Stock", 80, delta = -4)
     col3.metric("test", 100, delta = 10)
     col4.metric("test", 900, delta = -5)
     
+# Forecast and Portfolio Performance
 if tabs == "Forecast and Portfolio Performance":
     
     st.write("I am inevitable")
+    
+    
+if tabs == "Appendix":
+    
+    st.write("Put any clarifying information such as model type, assumptions ect...")
 
     
     
