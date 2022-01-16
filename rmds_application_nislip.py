9e4f7877e5cc47df88b16d1162522f35d49156c8
Nate Islip <39868505+nislip@users.noreply.github.com>
2022-01-16 08:53:40 -0800

    Update rmds_application_nislip.py

--- rmds_application_nislip.py
+++ rmds_application_nislip.py
@@ -12,6 +12,17 @@ if tabs == "EDA":
     st.subheader("Upload your Portfolio data: ")
     investor_data = st.file_uploader('Upload a CSV file')
     
+    st.subheader("Portfolio Overview")
+    
+    col1, col2, col3, col4 = st.columns(4) 
+    col1.metric("Most valued Stock", 70)
+    col1.metric("Least valued Stock", 80)
+    col1.metric("test", 100)
+    col1.metric("test", 900)
+    
 if tabs == "Forecast and Portfolio Performance":
     
     st.write("I am inevitable")
+
+    
+    
