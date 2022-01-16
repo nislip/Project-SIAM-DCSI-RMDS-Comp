14511a02011a72a42720ccc7888a663125777ca4
Nate Islip <39868505+nislip@users.noreply.github.com>
2022-01-16 08:30:46 -0800

    Update rmds_application_nislip.py

--- rmds_application_nislip.py
+++ rmds_application_nislip.py
@@ -3,11 +3,9 @@ import numpy as np
 import altair as alt
 import streamlit as st
 
-df = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
+st.title("RMDS Competiion")
 
-c = alt.Chart(df).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-
-st.altair_chart(c, use_container_width=True)
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
