1307d50eb12e464e73e0da528724a72d0066796c
Nate Islip <39868505+nislip@users.noreply.github.com>
2022-01-30 18:54:41 -0800

    Update rmds_application_nislip.py

--- rmds_application_nislip.py
+++ rmds_application_nislip.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
-import altair as alt
+from sklearn import cluster
 import streamlit as st
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import plotly.graph_objects as go
 
 st.title("RMDS Competiion")
 
@@ -15,6 +19,72 @@ def clean_dataset(df):
     df.dropna(inplace=True)
     indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
     return df[indices_to_keep].astype(np.float64)
+
+def KMEANS(data, featurenames):
+    '''
+    input data of funds 
+    featurenames will be used to subset the data 
+    Kmeans algorithm will be run with the default of 8 clusters 
+    return filtered data,  kmeans object. 
+    '''
+    df = data[featurenames]
+    # Running Kmeans
+    kmeans = KMeans(n_clusters=8).fit(df)
+    st.write('Kmeans is finished')
+    return df, kmeans
+
+def pca_model(kmeans):
+    '''
+    input is a kmeans object 
+    dictionary to track the clustering groups 
+    return pca objects for each clustering 
+    '''
+    mydict = {i: np.where(kmeans.labels_ == i)[0] for i in range(kmeans.n_clusters)}
+    pcaobjects = []
+    pca=PCA(n_components=3) # how many do we want? 
+    for key, value in mydict.items():
+        X_test = df[df.index.isin(list(value))] 
+        pcaobjects.append( pca.fit(X_test))
+    return pcaobjects
+
+def pca_loadings(group, pcaobjects, data):
+    '''
+    Graphs the bar charts for the loadings by the group number 
+    '''
+    df = data
+    
+    test = pcaobjects[group]
+    st.write(test.explained_variance_)
+    loadings = pd.DataFrame(test.components_.T, index = df.columns, columns = ['PC1', 'PC2', 'PC3']).reset_index()
+    loadings = loadings.reindex(loadings.PC1.abs().sort_values(ascending = False).index).head(5)
+    
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=loadings['index'],
+        y=loadings['PC1'],
+        name='Principal Component 1',
+        marker_color='Black'
+        ))
+    fig.add_trace(go.Bar(
+        x=loadings['index'],
+        y=loadings['PC2'],
+        name='Principal Component 2',
+        marker_color='Red'
+    ))
+    fig.add_trace(go.Bar(
+        x=loadings['index'],
+        y=loadings['PC3'],
+        name='Principal Component 3',
+        marker_color='Blue'
+    ))
+
+    fig.update_layout(barmode='group', xaxis_tickangle=-45)
+    st.plotly_chart(fig)
+
+# Containers
+search_container = st.beta_container()
+clustering_container = st.beta_container()
+
 
 data = pd.read_csv('funds.csv') # define the original dataframe
 
@@ -42,10 +112,10 @@ data = data.replace({'Sustainable Investment - Environmental Sector Fund' : yesN
 ## Convert Average Credit Quality to dictionary defined numerical value (ordered least to greatest).
 creditDict = {'B' : 0,
                 'BB' : 1,
-               'BBB' : 2,
-               'A' : 3,
-               'AA' : 4,
-               'AAA' : 5,}
+                'BBB' : 2,
+                'A' : 3,
+                'AA' : 4,
+                'AAA' : 5,}
 data = data.replace({'Average Credit Quality' : creditDict})               # Assign number to Average Credit Quality from dict
 
 ## Convert 'Equity StyleBox' to dictionary defined numerical value.
@@ -65,7 +135,11 @@ data['Average Market Cap (Mil)'] = data['Average Market Cap (Mil)'].str.replace(
 data['Fund Size (Mil)'] = data['Fund Size (Mil)'].astype(float)
 data['Average Market Cap (Mil)'] = data['Average Market Cap (Mil)'].astype(float)
 
+tickersnames = list(data['Ticker'])
+data = data.drop(columns = ['Name', 'Ticker']).reset_index()
 df = clean_dataset(df = data) # Define the complete data set
+df = df.drop(columns = ['index'])
+featurenames = df.columns
 
 #================== END DATA ======================
 ##=================================================
@@ -75,27 +149,33 @@ df = clean_dataset(df = data) # Define the complete data set
 
 # Exploratory Data Analysis
 if tabs == "Portfolio Summary":
-    
     # Portfolio Overview
+    with search_container:
+        st.subheader("Portfolio Overview")
+        
+        subsettickers = st.multiselect('Select a subset of tickers in your Portfolio', tickersnames) # Creates list
+        selectfeatures = st.multiselect('Select a subset of tickers to cluster', featurenames) # Creates list
+        
+    with clustering_container:
+        kmeans=None
+        if st.checkbox('Run Clustering Algorithm (Yes/No)'):
+            df, kmeans = KMEANS(df, selectfeatures)
+            # st.write(kmeans.n_clusters) # testing
+            option = st.selectbox("Choose clusters for pca: ", (0, 1, 2, 3, 4, 5, 6, 7)  )
+            pcaobjects = pca_model(kmeans)
+            pca_loadings(int(option), pcaobjects, df )
 
-    st.subheader("Portfolio Overview")
     
-    test = st.multiselect('Select a subset of tickers in your Portfolio', list(df['Ticker']))
- 
-    st.write("Output: View tickers (names) (1 visual)")
-    st.write("Input: Ticker(s) to view Price Per day (searchable drop down) ")
-    st.write("Output: Graph of Tickers (1 graph)")
-    st.write("Output: Grab list of tickers selected for timeseries. Display key column values for ticker i pause t seconds")
     
-    st.table(df.head(30))
+    #button = st.button('Run Clustering Algorithm (Yes/No)', on_click = KMEANS, arg = (df, selectfeatures))
     
     
     # Key Performance Indicators
-    col1, col2, col3, col4 = st.columns(4) 
-    col1.metric("Most valued Stock", 70, delta = 1)
-    col2.metric("Least valued Stock", 80, delta = -4)
-    col3.metric("test", 100, delta = 10)
-    col4.metric("test", 900, delta = -5)
+    #col1, col2, col3, col4 = st.columns(4) 
+    #col1.metric("Most valued Stock", 70, delta = 1)
+    #col2.metric("Least valued Stock", 80, delta = -4)
+    #col3.metric("test", 100, delta = 10)
+    #col4.metric("test", 900, delta = -5)
     
     # Graphing
     
@@ -108,6 +188,5 @@ if tabs == "Kmeans-PCA Model":
 if tabs == "Appendix":
     
     st.write("Put any clarifying information such as model type, assumptions ect...")
-
     
     
